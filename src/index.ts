a2082dede57cc6fd29e78dda9d99065eb4333dd9
David Maksimov <dmaksi@gmail.com>
2019-10-26 23:45:36 -0700

    eslint disable no-eval

--- src/index.ts
+++ src/index.ts
@@ -251,6 +251,7 @@ export const reviver = function reviver(options: Options) {
       const [, name, source] = value.match(/_function_([^|]*)\|(.*)/) || [];
 
       if (!options.lazyEval) {
+        // eslint-disable-next-line no-eval
         return eval(`(${source})`);
       }
 
